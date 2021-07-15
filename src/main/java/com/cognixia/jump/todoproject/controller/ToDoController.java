3ccc42e51a57cf6d5860096ea5d046c919c5a131
Jim Nguyen <vjimnguyen@gmail.com>
2021-07-15 08:07:20 -0700

    Fixed files

--- src/main/java/com/cognixia/jump/todoproject/controller/ToDoController.java
+++ src/main/java/com/cognixia/jump/todoproject/controller/ToDoController.java
@@ -6,10 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.Date;
 import java.util.List;
-
-import javax.validation.Valid;
 
 @RestController
 @RequestMapping(path = "/api")
